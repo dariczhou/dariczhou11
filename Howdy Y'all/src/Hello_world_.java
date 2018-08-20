558c63e5669ebe116f99d7b4823059c4a6ad04af
APCS3rd <APCS3rd@MHS-Math2-28>
2018-08-20 10:43:23 -0700

    First commit to GitHub

---
+++ Howdy Y'all/src/Hello_world_.java
@@ -0,0 +1,14 @@
+/**
+ * The first coding project 
+ * 
+ * @Daric Zhou
+ * @8/17/18
+ */
+public class Hello_world_
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Daric"); //Display the string
+    }
+}
+
