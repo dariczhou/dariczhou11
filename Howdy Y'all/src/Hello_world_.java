5a24d5c472ebd51a0045c20322c63b9fcdb0478f
Daric Zhou <dariczhou@gmail.com>
2018-08-21 22:24:21 -0700

    added some new code:)

--- Howdy Y'all/src/Hello_world_.java
+++ Howdy Y'all/src/Hello_world_.java
@@ -9,6 +9,11 @@ public class Hello_world_
     public static void main(String[] args)
     {
         System.out.println("Hello World! From Daric"); //Display the string
+        System.out.println("");
+        System.out.println("Coding is going swell..");	
+        System.out.println("I'm actually typing code");
+        System.out.println("...");
+        System.out.println("REEEEEEEEEEE");
     }
 }
 
